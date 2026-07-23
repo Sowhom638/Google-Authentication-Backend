5857bf50b1945a9badbd4b061c76d0cf6c05016a
Sowhom638 <sowhom.ghosh.22@aot.edu.in>
2026-07-23 11:32:40 +0530

    changed the sameSite

--- routes/auth.js
+++ routes/auth.js
@@ -38,7 +38,7 @@ router.post('/google', async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax', // ✅ 'none' for cross-site,
+      sameSite: 'none', // ✅ 'none' for cross-site,
       maxAge: 7 * 60 * 60 * 1000, // 7 hours
     });
 
