eec214c80032527b2326129fc926c125e83f924c
Sowhom638 <sowhom.ghosh.22@aot.edu.in>
2026-07-23 11:19:32 +0530

    Added sameSite condition

--- routes/auth.js
+++ routes/auth.js
@@ -38,7 +38,7 @@ router.post('/google', async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax', // ✅ 'none' for cross-site,
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
