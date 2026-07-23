0d3c7775759ac2f106138be9a9f24957c2524345
Sowhom638 <sowhom.ghosh.22@aot.edu.in>
2026-07-23 11:17:19 +0530

    httpsOnly -> httpOnly

--- routes/auth.js
+++ routes/auth.js
@@ -36,7 +36,7 @@ router.post('/google', async (req, res) => {
     );
 
     res.cookie('token', token, {
-      httpsOnly: true,
+      httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: 'lax',
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
